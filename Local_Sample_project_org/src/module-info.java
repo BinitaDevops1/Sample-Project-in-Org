781492f102f9a0fb4b8ca1eac53e8058dfe24dd2
BinitaSharma <binita.devops@outlook.com>
2025-05-21 15:52:17 -0500

    Commit to org

---
+++ Local_Sample_project_org/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Local_Sample_project_org {
+}
---
+++ Local_Sample_project_org/src/pack_1/Class1.java
@@ -0,0 +1,11 @@
+package pack_1;
+
+public class Class1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.print("This is my first class in org");
+
+	}
+
+}
